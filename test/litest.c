bba7e40a664c286d4213ba6bd3503aef31428132
Kieran Bingham <kieranbingham@gmail.com>
2015-07-11 21:53:04 -0500

    test: Fix compiler warning in litest
    Warning produced was:
    
    litest.c: In function 'litest_copy_file':
    litest.c:920:8: warning: ignoring return value of 'write', declared with attribute warn_unused_result [-Wunused-result]
       write(out, header, strlen(header));
            ^
    
    Slightly improve code by introducing an assert on the return value.

--- test/litest.c
+++ test/litest.c
@@ -921,12 +921,15 @@ static inline void
 litest_copy_file(const char *dest, const char *src, const char *header)
 {
 	int in, out;
+	int written;
 
 	out = open(dest, O_CREAT|O_WRONLY, 0644);
 	litest_assert_int_gt(out, -1);
 
-	if (header)
-		write(out, header, strlen(header));
+	if (header) {
+		written = write(out, header, strlen(header));
+		litest_assert_int_gt(written, 0);
+	}
 
 	in = open(src, O_RDONLY);
 	litest_assert_int_gt(in, -1);
